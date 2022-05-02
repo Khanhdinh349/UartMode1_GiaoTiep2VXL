62360b6223ad9498e45b9b71c914b42d3cca8eb7
Henry Nghiem <47719265+Khanhdinh349@users.noreply.github.com>
2022-05-02 09:02:04 +0700

    Add files via upload

---
+++ UartMode1_GiaoTiep2VXL/UartMode1_VXL1/main.c
@@ -0,0 +1,25 @@
+#include<main.h>
+#include<..\lib\UartMode1.h"
+#include<..\lib\Delay.h"
+
+void main()
+{
+	Uart_Init();
+
+	while(1)
+	{
+		if(P1_0==0)
+		{
+		 	Delay_ms(20);
+			Uart_Write(0x01);
+			while(P1_0==0);
+		}
+
+		if(P1_1==0)
+		{
+		 	Delay_ms(20);
+			Uart_Write(0x00);
+			while(P1_1==0);
+		}
+	}
+}
